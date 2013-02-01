f45cbe223969f974750dd4faec112f1e1a35aca7
Boyan Rabchev <boyan@rabchev.com>
2013-02-01 03:41:47 +0200

    Handling for select statement in cursor-mock.

--- test/mocks/cursor-mock.js
+++ test/mocks/cursor-mock.js
@@ -2,33 +2,46 @@
 /*global require, exports, module */
 
 var Cursor      = require("../../lib/cursor"),
-    util        = require("util");
+    util        = require("util"),
+    sift        = require("sift");
 
 function CursorMock(provider, query, projection, options) {
     Cursor.call(this, provider, query, projection, options);
     
     this.current = 0;
+    if (this.query) {
+        this.sifter = sift(this.query);
+    }
 }
 
 util.inherits(CursorMock, Cursor);
 
 CursorMock.prototype._isMatch = function (item) {
+    if (this.sifter) {
+        return this.sifter.test(item);
+    }
+    return true;
+};
+
+CursorMock.prototype._project = function (item) {
     
 };
 
 CursorMock.prototype._nextObject = function (callback) {
     "use strict";
     
-    var item = this.provider.store[this.current++];
-    while (item) {
-        if (this._isMatch(item)) {
-            break;
+    process.nextTick(function () {
+        var item = this.provider.store[this.current++];
+        while (item) {
+            if (this._isMatch(item)) {
+                break;
+            }
+            item = this.provider.store[this.current++];
         }
-        item = this.provider.store[this.current++];
-    }
-    
-    if (this.projection) {
-        item = this.project(item);
-    }
-    callback(item);
+        
+        if (this.projection) {
+            item = this._project(item);
+        }
+        callback(item);
+    });
 };
