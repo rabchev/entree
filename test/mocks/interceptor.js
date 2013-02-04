f77aa59d37f344096df04964eb21ab89b8b87a8c
Boyan Rabchev <boyan@rabchev.com>
2013-02-04 08:37:53 +0200

    Intecetors tests

--- test/mocks/interceptor.js
+++ test/mocks/interceptor.js
@@ -1,7 +1,9 @@
 /*jslint plusplus: true, devel: true, nomen: true, vars: true, node: true, sloppy: true, es5: true, indent: 4, maxerr: 50 */
 /*global require, exports, module */
 
-exports.security = function (action, context, itm, next, out) {
+exports.logdata = [];
+
+exports.security = function (action, context, item, next, out) {
     switch (action) {
     case "_update":
             
@@ -15,9 +17,20 @@ exports.security = function (action, context, itm, next, out) {
     case "_delete":
             
         break;
+    case "_select":
+            
+        break;
     }
 };
             
-exports.logging = function () {
+exports.logging = function (action, context, item, next, out) {
+    "use strict";
     
+    function handleResult(err, item) {
+        var msg = err ? "failed:" + err.message : "success";
+        exports.logdata.push({ action: action, message: msg });
+        out(err, item);
+    }
+    
+    next(item, handleResult);
 };
