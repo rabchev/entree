dd54f31218cc24224b117abb7a846e08249c0706
Boyan Rabchev <boyan@rabchev.com>
2013-12-08 17:53:50 +0200

    Fixed cache interceptor tests.

--- test/cache.js
+++ test/cache.js
@@ -2,32 +2,40 @@
 
 "use strict";
 
-function Store(name) {
-    this.name = name;
-    this.getCalls = 0;
-    this.setCalls = 0;
-    this.delCalls = 0;
-    this.store = {};
-}
+var store = {
+    stors: [],
+    create: function () {
+        var self = {};
 
-Store.prototype.get = function (key, callback) {
-    this.getCalls++;
-    callback(null, this.store[key]);
-};
+        self.index = this.stors.length;
+        self.getCalls = 0;
+        self.setCalls = 0;
+        self.delCalls = 0;
+        self.storage = {};
+        this.stors.push(self);
 
-Store.prototype.del = function (key, callback) {
-    this.delCalls++;
-    delete this.store[key];
-    if (callback) {
-        callback();
-    }
-};
+        self.get = function (key, callback) {
+            self.getCalls++;
+            callback(null, self.storage[key]);
+        };
 
-Store.prototype.set = function (key, val, callback) {
-    this.setCalls++;
-    this.store[key] = val;
-    if (callback) {
-        callback();
+        self.del = function (key, callback) {
+            self.delCalls++;
+            delete self.storage[key];
+            if (callback) {
+                callback();
+            }
+        };
+
+        self.set = function (key, val, callback) {
+            self.setCalls++;
+            self.storage[key] = val;
+            if (callback) {
+                callback();
+            }
+        };
+
+        return self;
     }
 };
 
@@ -35,9 +43,6 @@ var testCase        = require("nodeunit").testCase,
     Manager         = require("../lib/manager"),
     Provider        = require("./mocks/post-provider"),
     cache           = require("../lib/interceptors/cache"),
-    store1          = new Store("store1"),
-    store2          = new Store("store2"),
-    store3          = new Store("store3"),
     manager;
 
 module.exports = testCase({
@@ -46,7 +51,7 @@ module.exports = testCase({
 
         var provider    = new Provider({ connStr: "test connection string" }, { __collName: "test" });
 
-        provider.use(cache.interception({ store: store1 }, ["get", "delete", "select"]));
+        provider.use(cache.interception({ store: store }, ["get", "delete", "select"]));
 
         manager = new Manager();
         manager.addProvider(provider, "testProv", function (err) {
@@ -60,9 +65,9 @@ module.exports = testCase({
             test.ok(!err);
             test.ok(item);
             test.equal(manager.testProv.store["1"].name, "Foo");
-            test.equal(store1.setCalls, 0);
-            test.equal(store1.getCalls, 0);
-            test.equal(store1.delCalls, 0);
+            test.equal(store.stors[0].setCalls, 0);
+            test.equal(store.stors[0].getCalls, 0);
+            test.equal(store.stors[0].delCalls, 0);
 
             test.equal(manager.testProv.insertCalls, 1);
             test.equal(manager.testProv.updateCalls, 0);
@@ -77,10 +82,10 @@ module.exports = testCase({
         manager.testProv.get(1, function (err, item) {
             test.ok(!err);
             test.equal(item.name, "Foo");
-            test.equal(store1.setCalls, 1);
-            test.equal(store1.getCalls, 1);
-            test.equal(store1.delCalls, 0);
-            test.equal(store1.store["1"].name, "Foo");
+            test.equal(store.stors[0].setCalls, 1);
+            test.equal(store.stors[0].getCalls, 1);
+            test.equal(store.stors[0].delCalls, 0);
+            test.equal(store.stors[0].storage["1"].name, "Foo");
 
             test.equal(manager.testProv.insertCalls, 1);
             test.equal(manager.testProv.updateCalls, 0);
@@ -95,10 +100,10 @@ module.exports = testCase({
         manager.testProv.update({_id: 1, name: "Baz", age: 25 }, function (err, item) {
             test.ok(!err);
             test.equal(item.name, "Baz");
-            test.equal(store1.setCalls, 1);
-            test.equal(store1.getCalls, 1);
-            test.equal(store1.delCalls, 0);
-            test.equal(store1.store["1"].name, "Foo");
+            test.equal(store.stors[0].setCalls, 1);
+            test.equal(store.stors[0].getCalls, 1);
+            test.equal(store.stors[0].delCalls, 0);
+            test.equal(store.stors[0].storage["1"].name, "Foo");
 
             test.equal(manager.testProv.insertCalls, 1);
             test.equal(manager.testProv.updateCalls, 1);
@@ -113,9 +118,9 @@ module.exports = testCase({
         manager.testProv.get(1, function (err, item) {
             test.ok(!err);
             test.equal(item.name, "Foo");
-            test.equal(store1.setCalls, 1);
-            test.equal(store1.getCalls, 2);
-            test.equal(store1.delCalls, 0);
+            test.equal(store.stors[0].setCalls, 1);
+            test.equal(store.stors[0].getCalls, 2);
+            test.equal(store.stors[0].delCalls, 0);
             test.equal(manager.testProv.store["1"].name, "Baz");
 
             test.equal(manager.testProv.insertCalls, 1);
@@ -126,62 +131,62 @@ module.exports = testCase({
             test.done();
         });
     },
-//    "Add More Items and Select": function (test) {
-//        test.expect(17);
-//        var items = [
-//            {_id: 2, name: "Bar", age: 25 },
-//            {_id: 3, name: "Qux", age: 20 },
-//            {_id: 4, name: "Door", age: 30 },
-//            {_id: 5, name: "Red", age: 25 }
-//        ];
-//        manager.testProv.insert(items, function (err, items) {
-//            test.ok(!err);
-//            test.ok(items);
-//            test.equal(manager.testProv.store["2"].name, "Bar");
-//            test.equal(store1.setCalls, 1);
-//            test.equal(store1.getCalls, 2);
-//            test.equal(store1.delCalls, 0);
-//            manager.testProv.select({ age: 25 }, function (err, res) {
-//                test.ok(!err);
-//                res.toArray(function (err, arr) {
-//                    test.ok(!err);
-//                    test.ok(arr);
-//                    test.equal(arr.length, 3);
-//                    test.equal(store1.setCalls, 2);
-//                    test.equal(store1.getCalls, 3);
-//                    test.equal(store1.delCalls, 0);
-//
-//                    test.equal(manager.testProv.insertCalls, 2);
-//                    test.equal(manager.testProv.updateCalls, 1);
-//                    test.equal(manager.testProv.getCalls, 1);
-//                    test.equal(manager.testProv.selectCalls, 1);
-//
-//                    test.done();
-//                });
-//            });
-//        });
-//    },
-//    "Select from Cache": function (test) {
-//        test.expect(10);
-//
-//        manager.testProv.select({ age: 25 }, function (err, res) {
-//            test.ok(!err);
-//            test.equal(store1.setCalls, 2);
-//            test.equal(store1.getCalls, 4);
-//            test.equal(store1.delCalls, 0);
-//
-//            test.equal(manager.testProv.insertCalls, 2);
-//            test.equal(manager.testProv.updateCalls, 1);
-//            test.equal(manager.testProv.getCalls, 1);
-//            test.equal(manager.testProv.selectCalls, 1);
-//
-//            res.toArray(function (err, arr) {
-//                test.ok(!err);
-//                test.equal(arr.length, 3);
-//                test.done();
-//            });
-//        });
-//    },
+    "Add More Items and Select": function (test) {
+        test.expect(17);
+        var items = [
+            {_id: 2, name: "Bar", age: 25 },
+            {_id: 3, name: "Qux", age: 20 },
+            {_id: 4, name: "Door", age: 30 },
+            {_id: 5, name: "Red", age: 25 }
+        ];
+        manager.testProv.insert(items, function (err, items) {
+            test.ok(!err);
+            test.ok(items);
+            test.equal(manager.testProv.store["2"].name, "Bar");
+            test.equal(store.stors[0].setCalls, 1);
+            test.equal(store.stors[0].getCalls, 2);
+            test.equal(store.stors[0].delCalls, 0);
+            manager.testProv.select({ age: 25 }, function (err, res) {
+                test.ok(!err);
+                res.toArray(function (err, arr) {
+                    test.ok(!err);
+                    test.ok(arr);
+                    test.equal(arr.length, 3);
+                    test.equal(store.stors[0].setCalls, 2);
+                    test.equal(store.stors[0].getCalls, 3);
+                    test.equal(store.stors[0].delCalls, 0);
+
+                    test.equal(manager.testProv.insertCalls, 2);
+                    test.equal(manager.testProv.updateCalls, 1);
+                    test.equal(manager.testProv.getCalls, 1);
+                    test.equal(manager.testProv.selectCalls, 1);
+
+                    test.done();
+                });
+            });
+        });
+    },
+    "Select from Cache": function (test) {
+        test.expect(10);
+
+        manager.testProv.select({ age: 25 }, function (err, res) {
+            test.ok(!err);
+            test.equal(store.stors[0].setCalls, 2);
+            test.equal(store.stors[0].getCalls, 4);
+            test.equal(store.stors[0].delCalls, 0);
+
+            test.equal(manager.testProv.insertCalls, 2);
+            test.equal(manager.testProv.updateCalls, 1);
+            test.equal(manager.testProv.getCalls, 1);
+            test.equal(manager.testProv.selectCalls, 2);
+
+            res.toArray(function (err, arr) {
+                test.ok(!err);
+                test.equal(arr.length, 3);
+                test.done();
+            });
+        });
+    },
     "Throw on Select Without Callback": function (test) {
         test.expect(1);
         test.throws(function () {
@@ -194,7 +199,7 @@ module.exports = testCase({
 
         var provider    = new Provider({ connStr: "test connection string" }, { __collName: "multiCache" });
 
-        provider.use(cache.interception([{ store: store2 }, { store: store3 }]));
+        provider.use(cache.interception([{ store: store }, { store: store }]));
 
         manager.addProvider(provider, "multiCache", function (err) {
             test.ok(!err);
@@ -208,13 +213,13 @@ module.exports = testCase({
             test.ok(item);
             test.equal(manager.multiCache.store["1"].name, "Foo");
 
-            test.equal(store2.setCalls, 1);
-            test.equal(store2.getCalls, 0);
-            test.equal(store2.delCalls, 0);
+            test.equal(store.stors[1].setCalls, 1);
+            test.equal(store.stors[1].getCalls, 0);
+            test.equal(store.stors[1].delCalls, 0);
 
-            test.equal(store3.setCalls, 1);
-            test.equal(store3.getCalls, 0);
-            test.equal(store3.delCalls, 0);
+            test.equal(store.stors[2].setCalls, 1);
+            test.equal(store.stors[2].getCalls, 0);
+            test.equal(store.stors[2].delCalls, 0);
 
             test.equal(manager.multiCache.insertCalls, 1);
             test.equal(manager.multiCache.updateCalls, 0);
@@ -224,13 +229,13 @@ module.exports = testCase({
             manager.multiCache.get(1, function (err, item) {
                 test.ok(!err);
                 test.equal(item.name, "Foo");
-                test.equal(store2.setCalls, 1);
-                test.equal(store2.getCalls, 1);
-                test.equal(store2.delCalls, 0);
+                test.equal(store.stors[1].setCalls, 1);
+                test.equal(store.stors[1].getCalls, 1);
+                test.equal(store.stors[1].delCalls, 0);
 
-                test.equal(store3.setCalls, 1);
-                test.equal(store3.getCalls, 0);
-                test.equal(store3.delCalls, 0);
+                test.equal(store.stors[2].setCalls, 1);
+                test.equal(store.stors[2].getCalls, 0);
+                test.equal(store.stors[2].delCalls, 0);
 
                 test.equal(manager.multiCache.insertCalls, 1);
                 test.equal(manager.multiCache.updateCalls, 0);
