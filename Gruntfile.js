93b76e149669331443f5ffb431217835c9979b02
Boyan Rabchev <boyan@rabchev.com>
2013-12-13 00:50:25 +0200

    Added Grunt  debugging task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,10 @@
 "use strict";
 
 module.exports = function (grunt) {
+    var globalConfig = {};
+
     grunt.initConfig({
+        globalConfig: globalConfig,
         jsdoc : {
             dist : {
                 src     : ["./lib"],
@@ -16,7 +19,7 @@ module.exports = function (grunt) {
         test: {
             all         : "",
             fileSys     : "",
-            mongo       : "",
+            mongodb     : "",
             everlive    : "",
             manager     : "",
             entree      : "",
@@ -24,21 +27,40 @@ module.exports = function (grunt) {
         },
         nodeunit: {
             all         : ["*_test.js"],
-            fileSys     : ["file-system_test.js", "fs-common_test.js"],
-            mongo       : ["mo-common_test.js"],
-            everlive    : ["el-common_test.js"],
+            fileSys     : ["file-system_test.js", "fs-spec_test.js"],
+            mongodb     : ["mongodb_test.js"],
+            everlive    : ["everlive_test.js"],
             manager     : ["manager_test.js"],
             entree      : ["entree_test.js"],
             cache       : ["cache_test.js"]
+        },
+        shell: {
+            debugtest: {
+                options: {
+                    stdout      : true
+                },
+                command         : "node --debug-brk $(which grunt) test:<%= globalConfig.target %>"
+            }
+        },
+        "node-inspector"        : {
+            "default"               : {}
         }
     });
 
-    grunt.loadNpmTasks("grunt-release");
     grunt.loadNpmTasks("grunt-jsdoc");
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks("grunt-shell");
+    grunt.loadNpmTasks("grunt-release");
+    grunt.loadNpmTasks("grunt-contrib-nodeunit");
+    grunt.loadNpmTasks("grunt-node-inspector");
 
     grunt.registerMultiTask("test", function () {
         process.chdir("test");
         grunt.task.run(["nodeunit:" + this.target]);
+    });
+
+    grunt.registerTask("test-debug", function () {
+        process.chdir("test");
+        globalConfig.target = this.args[0];
+        grunt.task.run(["node-inspector", "shell:debugtest"]);
     });
 };
