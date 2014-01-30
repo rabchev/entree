6586c7424cd518e5fbd25d93181b56b6eaa9ac13
Boyan Rabchev <boyan@rabchev.com>
2014-01-30 18:23:08 +0200

    API

--- examples/blog-app/app.js
+++ examples/blog-app/app.js
@@ -4,20 +4,33 @@
 
 var entree      = require("entree"),
     authority   = require("authority"),
-    opts        = {
-        model:  {
-            providers: [{
-                "provider": "file-system",
-                "options": {
-                    "connStr": "./data"
-                },
-                "collections": [
-                    { "name": "blogs" }
-                ]
-            }]
-        }
-    };
+    logger      = entree.resolveInterceptor("log"),
+    connection1,
+    connection2,
+    schema,
+    options,
+    interceptor1,
+    interceptor2;
 
-entree.init(opts, function (err) {
+
+entree
+    .addConnection("fs-default", "file-system", "./data")
+    .addConnection("mongo-node:1", "mongodb", "mongodb://localhost/node1")
+    .addColleciton("users", "fs-default", [interceptor1, interceptor2], schema)
+    .addCollection("_odd-articles")
+        .setConnection("fs-default")
+        .setSchema(schema)
+        .use(logger({ level: "info", log: { action: true, query: true }}, ["insert", "delete"]))
+        .done(function (err) { console.log(err); })
+    .addCollection("_even-articles")
+        .setConnection("mongo-node:1")
+        .setSchema(schema)
+        .done()
+    .addReplicaSet("articles", ["-odd-articles", "-even-articles"], options)
+    .addShard("comments")
+        .setConnections([connection1, connection2])
+        .setSchema(schema);
+
+entree.users.get("blah@lbah.com", function (err, user) {
 
 });
